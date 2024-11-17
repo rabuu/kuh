31d450f99dfbce4295dcadd5322cdfabdbd7eb53
Rasmus Buurman <rasmus@rbuurman.de>
2024-11-17 02:01:18 +0100

    Introduce DataAnalysis module

---
+++ src/Kuh/DataAnalysis.hs
@@ -0,0 +1,20 @@
+module Kuh.DataAnalysis (chooseMode) where
+
+import Kuh (DataMode (..))
+import Data.Char (isDigit)
+
+-- | Choose the right data mode (as of now, Kanji is not supportet)
+chooseMode :: String -> DataMode
+chooseMode input
+  | isNumeric input = Numeric
+  | isAlphanumeric input = Alphanumeric
+  | otherwise = Binary
+
+isNumeric :: String -> Bool
+isNumeric = all isDigit
+
+alphanumericTable :: [Char]
+alphanumericTable = ['0'..'9'] ++ ['A'..'Z'] ++ " $%*+-./:"
+
+isAlphanumeric :: String -> Bool
+isAlphanumeric = all (`elem` alphanumericTable)
